265bfafaf62ec521a30206be2a4df66b667a1c7f
Manish Shaw <80380640+TheManishShaw@users.noreply.github.com>
2022-06-10 17:25:07 +0530

    fixed

--- pages/services/elements/ServicesDetails.js
+++ pages/services/elements/ServicesDetails.js
@@ -1,6 +1,6 @@
 import { Link } from "heroicons-react";
 
-export const ServicesDetails = () => {
+export  const ServicesDetails = () => {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="grid gap-10 lg:grid-cols-2">
