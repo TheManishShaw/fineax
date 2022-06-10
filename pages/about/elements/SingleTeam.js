2ce80c0c34a2ee6f5630202fcf40754dab2c0915
Manish Shaw <80380640+TheManishShaw@users.noreply.github.com>
2022-06-10 17:21:16 +0530

    error

--- pages/about/elements/SingleTeam.js
+++ pages/about/elements/SingleTeam.js
@@ -6,7 +6,7 @@ const SingleTeam = (props) => {
     <>
           <div  className="px-12 py-8 transition-colors duration-200 transform border cursor-pointer rounded-xl hover:border-transparent group hover:bg-blue-600 dark:border-gray-700 dark:hover:border-transparent">
               <div key={props.key} className="flex flex-col sm:-mx-4 sm:flex-row">
-                  <Image className="flex-shrink-0 object-covers rounded-full sm:mx-4 ring-4 ring-gray-300" width={24} height={24} src={props.img} alt={props.name} />
+                  <img className="flex-shrink-0 object-covers w-24 h-24 rounded-full sm:mx-4 ring-4 ring-gray-300" src={props.img} alt={props.name} />
 
                   <div className="mt-4 sm:mx-4 sm:mt-0">
                       <h1 className="text-xl font-semibold text-gray-700 capitalize md:text-2xl dark:text-white group-hover:text-white">{props.name}</h1>
