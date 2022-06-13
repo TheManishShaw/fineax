c9c34a207fffccc49d95d2e4787e4a5f64aecbde
Manish Shaw <80380640+TheManishShaw@users.noreply.github.com>
2022-06-13 11:01:32 +0530

    service details page added

--- pages/services/ServiceDetails.jsx
+++ pages/services/ServiceDetails.jsx
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import React from 'react'
 import ServicesDetails from './elements/ServicesDetails';
 import ServicesSection from './elements/ServicesSection';
 const ServiceDetails = () => {
   return (
     <div>
+        <Head>
+            <title> Service Details | Fineax</title>
+        </Head>
       <ServicesDetails />
       <ServicesSection/>
     </div>
