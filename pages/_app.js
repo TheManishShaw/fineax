c00817cc206ebd439ba7b058689c152b7a70f5b9
Manish Shaw <80380640+TheManishShaw@users.noreply.github.com>
2023-01-07 13:02:41 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <meta name="description" content="Fineax Data Solution company" />
-        <link rel="icon" href="/favicon.svg" />
+        <meta name="description" content="Fineax is one stop problem solving in artificial intelligence and machine learning systematically through a range of possible actions in order to reduce the distance of your predefined goal or solution." />
+        <meta name="keywords" content="Fineax,artificial intelligence, problem-solving,Speech/ Audio,Speech/ Audio,Computer Vision ">
+    <link rel="icon" href="/favicon.svg" />
       </Head>
       <Component {...pageProps} />
     </Layout>
